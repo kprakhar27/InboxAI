45d81d9677994f088561bf1295072b1ac0406fc7
Kumar Prakhar <kumarprkhr@gmail.com>
2025-02-07 16:34:47 -0500

    Add Readme and Update Nginx Conf

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,23 @@
-Test
+# InboxAI Backend
+
+## Description
+
+The backend of InboxAI is built using Flask, a lightweight WSGI web application framework in Python. It provides RESTful APIs for the frontend to interact with. The backend handles user authentication, data storage, and business logic. It uses SQLAlchemy for database interactions and Flask-JWT-Extended for handling JSON Web Tokens (JWT) for secure authentication. Additionally, Flask-CORS is used to enable Cross-Origin Resource Sharing (CORS) to allow the frontend to communicate with the backend seamlessly.
+
+The deployed apis can be used using the endpoint https://backend.inboxai.tech
+
+### Run Flask Backend
+
+* Run the following commands to run the flask application in the terminal
+* Make sure to cd into the **backend** directory
+
+```bash
+# Create virtual environment
+python -m venv ./venv 
+# Activate Virtual Environemt
+source venv/bin/activate
+# Install Requirements
+pip install -r requirements.txt 
+# Run App
+python app.py
+```
--- backend/add-nginx-wsgi-conf.sh
+++ backend/add-nginx-wsgi-conf.sh
@@ -14,8 +14,6 @@ else
 fi
 
 conf_text="server {
-    listen       80;
-    listen       [::]:80;
     server_name  backend.inboxai.tech;
     server_tokens off;
     root         $home_path;
@@ -35,6 +33,27 @@ conf_text="server {
     error_page 500 502 503 504 /50x.html;
         location = /50x.html {
     }
+
+
+    listen [::]:443 ssl ipv6only=on; # managed by Certbot
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/backend.inboxai.tech/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/backend.inboxai.tech/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+}
+server {
+    if ($host = backend.inboxai.tech) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+
+    listen       80;
+    listen       [::]:80;
+    server_name  backend.inboxai.tech;
+    return 404; # managed by Certbot
+
 
 }"
 
