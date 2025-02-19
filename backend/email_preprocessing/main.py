5ffd9f2fedf14709e681deffb3bb2a3cfe267438
Pradnyesh Choudhari <51751752+Prad06@users.noreply.github.com>
2025-02-19 02:24:11 -0500

    removing code from main which is not required

--- backend/email_preprocessing/main.py
+++ backend/email_preprocessing/main.py
@@ -1,15 +1,2 @@
 import os
 import sys
-
-from dotenv import load_dotenv
-
-load_dotenv("/Users/pradnyeshchoudhari/IE 7374 - LOCAL/InboxAI/backend/.env")
-
-# Add the project directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-email = "learningg951@gmail.com"
-from pipelines.preprocessing_pipeline import PreprocessingPipeline
-
-pipeline = PreprocessingPipeline(email)
-pipeline.process_ready_items()
