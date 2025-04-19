ab1498f98755ce413e34f039e664ea3973675b88
Pradnyesh Choudhari <51751752+Prad06@users.noreply.github.com>
2025-04-19 02:45:15 -0400

    changing email loop

--- data_pipeline/airflow/dags/tasks/email_fetch_tasks.py
+++ data_pipeline/airflow/dags/tasks/email_fetch_tasks.py
@@ -435,12 +435,11 @@ def send_success_email(**context):
 def send_monitoring_email(**context):
     """Send a monitoring email notification."""
     logger.info("Starting send_monitoring_email")
-    logger.info(f"Context: {context}")
-    # Check if the context has the key 'has_alert'
-    # and if its value is True
-    logger.info(context.get("has_alert"))
+    ti = context["task_instance"]
+    has_alert = ti.xcom_pull(key="has_alert")
+    logger.info(f"Has alert from xcom: {has_alert}")
 
-    if not context.get("has_alert"):
+    if not has_alert:
         logger.info("No alerts present.")
         return True
 
