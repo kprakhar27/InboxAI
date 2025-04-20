6659f3888f300ce7ae791561f2d0cc7db0be9fde
Pradnyesh Choudhari <51751752+Prad06@users.noreply.github.com>
2025-04-19 22:01:27 -0400

    adding github actions for deployment

---
+++ vector_db/setup.sh
@@ -0,0 +1,16 @@
+home_vector_path=$(pwd)/vector_db
+vector_path=~/vector-database
+
+if [ -f $vector_path/docker-compose.yaml ]
+then
+    echo "yes"
+    docker-compose -f $vector_path/docker-compose.yaml down
+    rm -rf $vector_path
+    mkdir $vector_path
+    cp -r $home_vector_path/* $vector_path
+else
+    echo "no"
+    cp -r $home_vector_path/* $vector_path
+fi
+
+docker-compose -f $vector_path/docker-compose.yaml up --build -d
